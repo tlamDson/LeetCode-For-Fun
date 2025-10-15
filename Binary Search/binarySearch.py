0fe973620bc40254369b3c11b20fd899da97dc18
Lam Pham <phamla@dickinson.edu>
2025-10-15 17:22:58 -0400

    Implement binary search

--- Binary Search/binarySearch.py
+++ Binary Search/binarySearch.py
@@ -1,10 +1,16 @@
 def binarySearch(nums,target):
-    for index,num in enumerate(nums):
-        print(num,index)
-
-        if num == target:
-            return index
+    # fisrt have the left and right bound
+    left,right = 0, len(nums)-1
+    #why right is len(nums) -1 but now len(nums) because the index starts at 0 so the last valid index is equal to the total length(included 0) minus 1
+    #now we implement the core of the functions
+    while left <= right:
+        mid = (left + right) //2 # need to take the mod because we cannot compare a float to an int
+        if nums[mid] == target:
+            return mid # we return the index
+        elif nums[mid] < target:
+            left = mid + 1 # if nums[mid] < target it means that everything from the left is smaller than the value at mid so do not need to search for the value of mid
+        else:
+            right = mid -1
     return -1
-        
-nums = [-1,0,3,5,9,12]
-binarySearch(nums,9)
+numbers = [1, 3, 5, 7, 9, 11]
+print(binarySearch(numbers,3))
