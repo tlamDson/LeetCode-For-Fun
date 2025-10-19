9a578507c7ccbd1f4d50826bc7b4d1409d6f60d0
Lam Pham <phamla@dickinson.edu>
2025-10-19 18:21:18 -0400

    Explain two sum to a friend

--- Array & Hashing/twosum.py
+++ Array & Hashing/twosum.py
@@ -1,9 +1,11 @@
-def twoSum(nums,target):
+def twoSum( nums, target):
     my_dict = {}
     for index,num in enumerate(nums):
         complement = target - num
-        if (complement in my_dict):
+        if complement in my_dict:
             return [my_dict[complement],index]
         my_dict[num] = index
-        print(my_dict)
-        return []
+    return []
+     
+nums = [4,5,6]
+print(twoSum(nums,10))
